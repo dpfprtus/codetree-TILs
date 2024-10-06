2419517cae2f10efad6fe89fab5efc59b70a8c8e
최수용 <dpfprtus@gmail.com>
2024-10-06 18:31:41 +0900

    Title: 루돌프의 반란, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241006/루돌프의 반란/rudolph-rebellion.py
+++ 241006/루돌프의 반란/rudolph-rebellion.py
@@ -126,6 +126,7 @@ def rudolf_move(ik):
                     if santa[k][0] == r and santa[k][1] == c:
                         santa[i][0] += dr[santa[k][2]]
                         santa[i][1] +=  dc[santa[k][2]]
+                        santa[i][2] = santa[k][2]
                         k = i
                         flag = 1
                         #죽으면
