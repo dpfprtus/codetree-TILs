41dfa38f3f9f4e9514e43cbc936753e7ac0738c8
최수용 <dpfprtus@gmail.com>
2024-10-25 17:44:24 +0900

    Title: 그 계절, 그 날, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241025/그 계절, 그 날/that-season-that-day.java
+++ 241025/그 계절, 그 날/that-season-that-day.java
@@ -52,7 +52,7 @@ public class Main {
         if(y % 4 == 0 && y % 100 == 0){
             return false;
         }
-        if(y%4 == 0 && y % 100 == 0 && y % 400 == 0){
+        if((y%4 == 0 && y % 100 == 0) && y % 400 == 0){
             return true;
         }
         if(y%4 == 0){
