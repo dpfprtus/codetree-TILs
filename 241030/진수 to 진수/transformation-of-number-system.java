a8f047140816ea32020ef244e169e243de2cf532
최수용 <dpfprtus@gmail.com>
2024-10-30 22:21:51 +0900

    Title: 진수 to 진수, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241030/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+        int b = in.nextInt();
+        String n = in.next();
+
+        int decem = 0;
+        int cnt = 0;
+        for(int i = n.length()-1;i >= 0;i--){
+            
+            decem += Math.pow(a,i)*Character.getNumericValue(n.charAt(cnt));
+            cnt += 1;
+        }
+
+        String answer = "";
+        while(decem != 0){
+            answer += Integer.toString(decem%b);
+            decem /= b;
+            
+        }
+        System.out.print(answer);
+
+    }
+}
