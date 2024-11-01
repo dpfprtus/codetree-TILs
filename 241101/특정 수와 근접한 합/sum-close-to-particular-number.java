a32a1c36a6f5737a783e794b4ccc1971cae183a9
최수용 <dpfprtus@gmail.com>
2024-11-01 16:48:07 +0900

    Title: 특정 수와 근접한 합, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241101/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Main {
+
+    public static int[] visited;
+    public static int[] nums;
+    public static int[] args;
+    public static int maxNum = 100000;
+    public static void main(String[] axrgs) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int s = in.nextInt();
+
+        nums = new int[n];
+
+        for(int i = 0; i< n;i++){
+            nums[i] = in.nextInt();
+        }
+        
+        visited = new int[n];
+        args = new int[n];
+        generate(0,s,n);
+        System.out.print(maxNum);
+        
+    }
+
+    
+
+    public static void generate(int x,int s,int n){
+        if(x == 2){
+            int result = 0;
+            for(int i = 0;i<args.length;i++){
+                result += args[i];
+            }
+            result -= s;
+            if(result>=0 && maxNum >= result){
+                maxNum = result;
+            }
+            return;
+
+        }
+
+        for(int i = 0;i<n;i++){
+            if(visited[i] == 0){
+                visited[i] = 1;
+                args[i] = nums[i];
+                generate(x+1,s,n);
+                visited[i] = 0;
+            }
+        }
+    }
+}
