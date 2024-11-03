9c3531312bfb079e29956ac6cbde4cfd5d7e8020
최수용 <dpfprtus@gmail.com>
2024-11-03 16:33:51 +0900

    Title: 스승의 은혜 3, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241103/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 241103/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 
 public class Main {
     public static int N,B;
-    public static int answer = Integer.MAX_VALUE;
+    public static int answer = 0;
     public static int[] gift,deliver;
     public static int flag = 0;
 
@@ -27,71 +27,36 @@ public class Main {
 
         int[] a = new int[N];
         for(int i = 0;i<N;i++){
-            a[i] = i;
-        }
-        cal(a);
-        if(flag == 1){
-            System.out.print(answer);
-            return;
-        }
-        
-        for(int i = N-1;i >= 1;i--){
-            int[] ans = new int[i];
-            int[] visited = new int[N];
-            dfs(0,ans,visited);
-            if(flag == 1)
-                break;
+
+            int tmp[] = new int[N];
+
+            for(int j = 0;j<N;j++){
+                tmp[j] = gift[j];
+            } 
+
+            tmp[i] /= 2;
+
+            int[] sumList = new int[N];
+
+            for(int j = 0;j<N;j++){
+                sumList[j] = (tmp[j]+deliver[j]);
+            }
+
+            Arrays.sort(sumList);
+
+            int student = 0;
+            int tmp1 = 0;
+
+            for(int k = 0;k<N;k++){
+                if(tmp1 +  sumList[k] > B)
+                    break;
+                tmp1 += sumList[k];
+                student++;
+            }
+            answer = Math.max(answer,student);
+            
         }
         System.out.print(answer);
     }
-
-    public static void dfs(int x, int[] ans,int[] visited){
-        if(x == ans.length){
-            cal(ans);
-            return;
-        }
-        if(flag == 1){
-            return;
-        }
-        for(int i = 0;i<N;i++){
-            for(int j = 0;j<ans.length;j++){
-                if (visited[i] == 0){
-                    visited[i] = 1;
-                    ans[j] = i;
-                    dfs(x+1,ans,visited);
-                    visited[i] = 0;
-                    
-                }
-            }
-            
-        }
-    }
-
-    public static boolean cal(int[] ans){
-        int result = Integer.MAX_VALUE;
-        
-        for(int i = 0;i<ans.length;i++){
-          
-            int tmp = 0;
-            for(int j = 0;j<ans.length;j++){
-                if(i == j){
-                    tmp += (gift[ans[j]]/2+deliver[ans[j]]);
-                }
-                else{
-                    tmp += (gift[ans[j]]+deliver[ans[j]]);
-                }
-            }
-            result = Math.min(result,tmp);
-
-            if(result <= B){
-                answer = ans.length;
-                flag = 1;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    
 
 }
